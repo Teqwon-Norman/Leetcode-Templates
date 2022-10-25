3136ebe9085c8d5f510bce97baccde8aaaca83db
Teqwon Norman <teqwon.norman@outlook.com>
2022-10-25 09:19:27 -0400

    fixed binary-search template

--- binary-search/main.py
+++ binary-search/main.py
@@ -1,11 +1,9 @@
 def binarySearch(nums: list[int], target: int) -> int:
     l, r = 0, len(nums)
-
     while l < r:
         m = (l + r) // 2
         if nums[m] < target:
             l = m + 1
-
         else:
             r = m
     return l
