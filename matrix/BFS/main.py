f4a8ba1290c31a5624168198f395474a13182cd8
Teqwon Norman <teqwon.norman@outlook.com>
2023-01-09 17:23:59 -0500

    fixed BFS template

--- matrix/BFS/main.py
+++ matrix/BFS/main.py
@@ -1,13 +1,27 @@
 from collections import deque
 
+"""
+Most matrix problems require you to find a range of consistent values
+such as the famous problems involving islands, this is a great template
+"""
+
+def solve(grid: list[list[int]]):
+    row, col = len(grid), len(grid[0])
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c] == 1:
+                bfs(grid, r, c)
+
 def bfs(grid: list[list[int]], x: int, y: int):
     q = deque([(x, y)])
-    visited = set([(x, y)])
 
     while q:
         x, y = q.popleft()
-        
         for row, col in [(x, y), (x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
-            if row > 0 or col > 0 or row <= len(grid) or col <= len(grid[0]) or (row, col) not in visited:
-                visited.add((row, col))
+            if inbounds(grid, row, col) or (row, col) == 1:
                 q.append((row, col))
+
+def inbounds(grid, r, c):
+    if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+        return True
+    return False
